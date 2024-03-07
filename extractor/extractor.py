dfb107b73c0f20dbbadd9e68b763a8976f40351c
maxearle <121108048+maxearle@users.noreply.github.com>
2024-03-07 13:03:41 +0000

    V2.0

--- extractor/extractor.py
+++ extractor/extractor.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     app = QApplication(sys.argv)
     model = Model()
-    w = MainWindow(title = "Extractor v2.0gamma")
+    w = MainWindow(title = "Extractor v2.0")
     ctrlr = Controller(w,model)
     app.exec()
